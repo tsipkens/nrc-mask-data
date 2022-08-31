452e5e8c2a3f4a3475547fa58e372f846bb16206
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-08-31 12:52:13 -0400

    Refactor joeh22/main.m
    - Reduce unnecessary code.
    - Add comments throughout.

--- joeh22/main.m
+++ joeh22/main.m
@@ -6,40 +6,48 @@ clear;
 close all;
 clc;
 
+% Colormap used for some plots.
+cm = inferno(300);
+cm = [0, 1, 0; cm];  % add green at the beginning of the colormap
 
-% Do some data analysis. 
-s = tools.json_read('sipkens22.json');
+% Read data and format.
+s = tools.json_read('sipkens22.json');  % read as Matlab structure
 s = db.fillnan(s);
 d = s(1).dm;
 
-% s = s([359, 134, 371, 643, 649, 297, 622, 643]);
-
-
+% Get mass-mobility information (requires autils).
 prop = massmob.init('salt');  % get mass-mobility parameters
-prop_alt = massmob.init('zet', 2.97, 'rho100', 1940);
 
+%== INITIAL PROCESSING ===================================================%
 % Compute integrated quantities.
-[dg_up, sg_up] = get_geo([s.smps1], [s.dm]);
-[dg_down, sg_down] = get_geo([s.smps2], [s.dm]);
-n_ni = pfe.npfe([s.smps1], [s.smps2]);
-m_ni = pfe.mpfe_ni([s.smps1], [s.smps2], [s.dm], prop);
-m_hc = pfe.mpfe_hc([s.smps1], [s.smps2], [s.dm], prop);
-m_hc2 = pfe.mpfe_hc([s.smps1], [s.smps2], [s.dm], prop_alt);
+n_ni = pfe.npfe([s.smps1], [s.smps2]);  % NPFE
+m_ni = pfe.mpfe_ni([s.smps1], [s.smps2], [s.dm], prop);  % MPFE (numerical integration)
+m_hc = pfe.mpfe_hc([s.smps1], [s.smps2], [s.dm], prop);  % MPFE (Hatch-Choate)
 
+% Compute scattering-based PFE.
 ri = 1.54 + 0j;  % particle refractive index (salt)
-int = mie.get_intensity(532e-9, [s.dm] .* 1e-9, ri, [], 45/180*pi);
-sca_45 = pfe.scapfe_ni([s.smps1], [s.smps2], int);
+int = mie.get_intensity(532e-9, [s.dm] .* 1e-9, ri, [], 45/180*pi);  % use Mie theory (requires autils)
+sca_45 = pfe.scapfe_ni([s.smps1], [s.smps2], int);  % use numerical integration to compute PFE
 
+% Get SPFE and PFE at dm = 100 nm.
 [sp, ssp] = pfe.spfe([s.smps1], [s.smps2], ...
     [s.se1] ./ sqrt([s.N_SMPS1_xN]), [s.se2] ./ sqrt([s.N_SMPS2_xN]), 1);
 pfe_100 = pfe.spfe_d([s.smps1], [s.smps2], [s.dm], 100);
 
 [~, ssp1] = pfe.spfe([s.smps1], [s.smps2], ...
-    [s.se1], [s.se2], 1);
-dmpps = tools.get_mpps(sp, [s.dm], ssp1);
-% dmpps = tools.hatch2mpps(dg_up, sg_up, dg_down, sg_down);
+    [s.se1], [s.se2], 1);  % SPFE with different uncertainty intervals than above
+dmpps = tools.get_mpps(sp, [s.dm], ssp1);  % get most-penetrating particle size (MPPS)
 
+%{
+% Alternative MPPS calc.
+[dg_up, sg_up] = get_geo([s.smps1], [s.dm]);  % get geometric moments (upstream)
+[dg_down, sg_down] = get_geo([s.smps2], [s.dm]);  % get geometric moments (downstream)
+dmpps = tools.hatch2mpps(dg_up, sg_up, dg_down, sg_down);
+%}
 
+color = max(log10(min(dmpps, 500)), 1);  % for coloring points below (capped on either end)
+
+% Convert to penetration and copy back to structure.
 for ii=1:length(s)
     s(ii).m_hc = 1 - m_hc(ii);
     s(ii).n_ni = 1 - n_ni(ii);
@@ -48,83 +56,53 @@ for ii=1:length(s)
 end
 
 
-% Percent error relative to zet = 3.
-pe_zet = (-m_hc2 + m_hc) ./ (1 - m_hc);
-figure(1);
-subplot(3, 1, [2,3])
-plot(1 - n_ni, 1 - m_hc, '.');
-hold on;
-plot(1 - n_ni, 1 - m_hc2, 'o', 'MarkerSize', 4);
-plot([0.01,1], [0.01, 1]);
-hold off;
-set(gca, 'XScale', 'log', 'YScale', 'log', ...
-    'XDir', 'reverse', 'YDir', 'reverse');
-xlabel('Number-based pen.');
-ylabel('Mass-based pen.');
-lim_x = xlim();
-
-subplot(3, 1, 1);
-plot(1 - m_hc, 1 - pe_zet, '.');
-set(gca, 'XScale', 'log', 'XDir', 'reverse');
-title('Effect of mass-mobility exponent');
-ylabel('Fractional difference');
-xlim(lim_x);
-
-
 %%
-% Plot NPFE v. MPFE. 
+% FIG 2: Plot NPFE v. MPFE. 
 figure(2);
-cl = max(log10(min(dmpps, 500)), 1);
-scatter(1 - n_ni, 1 - m_hc, 20, cl, 'filled');
+scatter(1 - n_ni, 1 - m_hc, 20, color, 'filled');
 hold on;
-% plot(1 - n_ni, 1 - m_ni, 'ok', 'MarkerSize', 4)
-% plot(1 - n_ni, 1 - m_hc2, 'ok', 'MarkerSize', 4);  % for alternate zet
 plot([0.01,1], [0.01, 1], 'k');
 hold off;
 
 set(gca, 'XScale', 'log', 'YScale', 'log', ...
     'XDir', 'reverse', 'YDir', 'reverse');
-xlim([1e-3, 1.5]);
-ylim([3e-4, 1.5]);
+xlim([1e-3, 1.5]);  ylim([3e-4, 1.5]);
 title('NPFE v. MPFE');
 xlabel('Number-based pen.');
 ylabel('Mass-based pen.');
 
-cm = inferno(300);
-cm = [0,1,0;cm];
 colormap(cm);
 colorbar;
 
-
-% Compare mass- and scattering-based PFEs.
+%%
+% FIG 3: Compare mass- and scattering-based PFEs.
 figure(3);
-scatter(1 - m_hc, 1 - sca_45, 20, cl, 'filled');
+scatter(1 - m_hc, 1 - sca_45, 20, color, 'filled');
 hold on;
 plot([0.01,1], [0.01, 1], 'k');
 hold off
 
-xlim([1e-3, 1.5]);
-ylim([3e-4, 1.5]);
+xlim([1e-3, 1.5]);  ylim([3e-4, 1.5]);
 set(gca, 'XScale', 'log', 'YScale', 'log', ...
     'XDir', 'reverse', 'YDir', 'reverse');
-title('Scattering and mass-based PFEs');
+title('MPFE v. Scattering-based PFE');
+xlabel('Mass-based pen.');
+ylabel('Scattering-based pen.')
 
-cm = inferno(300);
-cm = [0,1,0;cm];
 colormap(cm);
 colorbar;
 
-
 %%
+% FIG 4: SPFE for subset of data with known properties.
 figure(4);
-f1 = cl > 2.1;
-f2 = cl < 1.8;
-m1 = median(real(log(1 - sp(:,f1))), 2, 'omitnan');
-s1l = prctile(real(log(1 - sp(:,f1))), 25, 2);
-s1u = prctile(real(log(1 - sp(:,f1))), 75, 2);
-m2 = median(real(log(1 - sp(:,f2))), 2, 'omitnan');
-s2l = prctile(real(log(1 - sp(:,f2))), 25, 2);
-s2u = prctile(real(log(1 - sp(:,f2))), 75, 2);
+fh = color > 2.1;  % flag high MPPS
+fl = color < 1.8;  % flag low MPPS
+m1 = median(real(log(1 - sp(:,fh))), 2, 'omitnan');
+s1l = prctile(real(log(1 - sp(:,fh))), 25, 2);
+s1u = prctile(real(log(1 - sp(:,fh))), 75, 2);
+m2 = median(real(log(1 - sp(:,fl))), 2, 'omitnan');
+s2l = prctile(real(log(1 - sp(:,fl))), 25, 2);
+s2u = prctile(real(log(1 - sp(:,fl))), 75, 2);
 
 plot(d, exp(m1), 'k');
 hold on;
@@ -134,39 +112,41 @@ plot(d, exp(m2), 'k');
 plot(d, exp(s2l), '--');
 plot(d, exp(s2u), '--');
 hold off;
+
 set(gca, 'XScale', 'log', 'YScale', 'log', ...
     'XDir', 'normal', 'YDir', 'reverse');
 xlim([22, 320]);
+title('SPFE for data subset');
 
-
-figure(6);
-scatter(1 - m_hc, 1 - pfe_100, 20, cl, 'filled');
+%%
+% FIG 5: SPFE @ 100 nm v. MPFE.
+figure(5);
+scatter(1 - m_hc, 1 - pfe_100, 20, color, 'filled');
 hold on;
 plot([0.01,1], [0.01, 1], 'k');
 hold off;
-set(gca, 'XScale', 'log', 'YScale', 'log', ...
-    'XDir', 'reverse', 'YDir', 'reverse');
 
+% Get correlation between MPFE and SPFE @ 100 nm.
 A = 1 - [pfe_100', m_hc'];
 A(any(isnan(A), 2), :) = [];
 A(any(isinf(A), 2), :) = [];
 R2 = corrcoef(A(:, 1), A(:, 2))
 
+set(gca, 'XScale', 'log', 'YScale', 'log', ...
+    'XDir', 'reverse', 'YDir', 'reverse');
 xlim([1e-3, 1.5]);
 ylim([3e-4, 1.5]);
-title('PFE @ 100 v. MPFE');
+title('SPFE @ 100 v. MPFE');
 ylabel('Pen. @ 100 nm');
 xlabel('Mass-based pen.');
 
-cm = inferno(300);
-cm = [0,1,0;cm];
 colormap(cm);
 colorbar;
 
 
 %%
-
-figure(5);
+% FIG 6: Compare SMPS-based and CPC NPFE.
+figure(6);
 plot(1 - n_ni, [s.N_CPC2_xAvg] ./ [s.N_CPC1_xAvg], '.');
 hold on;
 plot([0.01,1], [0.01, 1], 'k');
@@ -174,43 +154,41 @@ hold off;
 set(gca, 'XScale', 'log', 'YScale', 'log', ...
     'XDir', 'reverse', 'YDir', 'reverse');
 
+% Get correlation between SMPS-based and CPC NPFE.
 A = 1 - [n_ni', [s.N_CPC2_xAvg]' ./ [s.N_CPC1_xAvg]'];
 A(any(isnan(A), 2), :) = [];
 A(any(isinf(A), 2), :) = [];
 R1 = corrcoef(A(:, 1), A(:, 2))
 
 title('CPC v. SMPS NPFE');
-xlabel('SMPS number-based pen.');
+ylabel('SMPS number-based pen.');
 xlabel('CPC number-based pen.');
 
 
-
 %%
-% TSI v. PFEMS
-sin_f = db.combine(s, 'lot', 'm_hc');
-
+% FIG 7: TSI v. PFEMS
 figure(7);
 
-errorbar([sin_f.m_hc], [sin_f.TSI1], ...
-    [sin_f.m_hc_min] - [sin_f.m_hc], [sin_f.m_hc_max] - [sin_f.m_hc], '.', 'horizontal');
+s_f = db.combine(s, 'lot', 'm_hc');  % average over the lots
+
+errorbar([s_f.m_hc], [s_f.TSI1], ...
+    [s_f.m_hc_min] - [s_f.m_hc], [s_f.m_hc_max] - [s_f.m_hc], 'o', 'horizontal');
 hold on;
-errorbar([sin_f.m_hc], [sin_f.TSI2], ...
-    [sin_f.m_hc_min] - [sin_f.m_hc], [sin_f.m_hc_max] - [sin_f.m_hc], '.', 'horizontal');
-% plot(m_hc, [sin.TSI1], '.b');
-% plot(m_hc, [sin.TSI2], '.r');
+errorbar([s_f.m_hc], [s_f.TSI2], ...
+    [s_f.m_hc_min] - [s_f.m_hc], [s_f.m_hc_max] - [s_f.m_hc], 'o', 'horizontal');
 plot([0.01,1], [0.01, 1], '-k');
 hold off;
 set(gca, 'XScale', 'log', 'YScale', 'log', ...
     'XDir', 'reverse', 'YDir', 'reverse');
 
-sin_f2 = db.filter(sin_f, 'id', 'Green*');
+% For Greenline samples (add as a star).
+s_green = db.filter(s_f, 'id', 'Green*');  % filter for Greenline
 hold on;
-% plot([sin_f.m_hc], [sin_f.TSI1], 'or');
-% plot([sin_f.m_hc], [sin_f.TSI2], 'ob');
-plot([sin_f2.m_hc], [sin_f2.TSI2], 'kp', 'MarkerSize', 10);
+plot([s_green.m_hc], [s_green.TSI2], 'kp', 'MarkerSize', 10, 'LineWidth', 2);  % add this point
 hold off;
 
-A = [[sin_f.m_hc]', [sin_f.TSI1]'; [sin_f2.m_hc]', [sin_f2.TSI2]'];
+% Compute correlation between quantities. 
+A = [[s_f.m_hc]', [s_f.TSI1]'; [s_green.m_hc]', [s_green.TSI2]'];
 A(any(isnan(A), 2), :) = [];
 A(any(isinf(A), 2), :) = [];
 R3 = corrcoef(A(:, 1), A(:, 2))
@@ -219,51 +197,3 @@ title('PFEMS MPFE v. TSIs');
 xlabel('PFEMS mass-based pen.');
 ylabel('TSI mass-based pen.');
 
-
-%{
-%-- Weight/charge state specific plots -----%
-figure(10);
-clf;
-vec1 = {'H', 'MH', 'ML', 'L'};
-vec2 = {'H', 'M', 'N'};
-sym = {'o', '<', '>', 's'};
-cl2 = {'g', 'r', 'b'};
-hold on;
-for ii=1:length(vec1)
-    for jj=1:length(vec2)
-        sij = db.filter(s, 'lot', ['R.', vec1{ii}, '.', vec2{jj}, '*']);
-        plot([sij.n_ni], [sij.m_hc], [sym{ii}, cl2{jj}]);
-    end
-end
-set(gca, 'XScale', 'log', 'YScale', 'log', ...
-    'XDir', 'reverse', 'YDir', 'reverse');
-hold on;
-plot([0.1, 1], [0.1, 1], 'k-');
-hold off;
-
-
-figure(11);
-clf;
-hold on;
-for ii=1:length(vec2)
-    sij = db.filter(s, 'lot', ['R.*.', vec2{ii}, '.*']);
-    fd = and(~isnan(prctile([sij.sp], 25, 2)), ...
-        ~isnan(prctile([sij.sp], 75, 2)));
-    
-    p25 = prctile([sij.sp], 25, 2);
-    p75 = prctile([sij.sp], 75, 2);
-    area([sij(1).dm(fd), fliplr(sij(1).dm(fd))], ...
-        [p25(fd), fliplr(p75(fd) - p25(fd))]);
-    
-    sij = db.filter(s, 'lot', ['R.*.', vec2{ii}, '.*']);
-    plot(sij(1).dm, nanmean([sij.sp], 2), [cl2{ii}, '-']);
-end
-sgl = db.filter(s, 'lot', 'Greenline');
-sgl = sgl([2,3]);
-plot(sij(1).dm, nanmean([sgl.sp], 2), 'k-')
-hold off;
-set(gca, 'XScale', 'log', 'YScale', 'log', 'YDir', 'reverse');
-ylim([4e-2, 1]);
-xlim([22, 320]);
-%-----------------------------------------------------%
-%}
